a9e703df65395f189b5bb59a2322302f4d426e68
Leekyujin <kyujinlee82@gmail.com>
2022-08-08 16:48:04 +0900

    기본적인 게시물 작성, 리스트 구현

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -7,21 +7,58 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int lastArticleId = 0;
+
 		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine().trim();
-			
+
 			if (cmd.equals("exit")) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
 			}
 
-			if (cmd.equals("article list")) {
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				lastArticleId++;
+
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+			} else if (cmd.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+
 				System.out.println("== 게시물 목록 ==");
-				
+				System.out.println(" 번호   |   제목");
+
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+
+					System.out.printf("%4d    |   %4s\n", article.id, article.title);
+				}
+
+			} else {
+				System.out.println("존재하지 않는 명령어입니다.");
 			}
 		}
+		System.out.println("==프로그램 끝==");
+		sc.close();
 	}
 }
