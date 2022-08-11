5738a938c5e154ce3770dbdffa6034ecc1046e5c
Leekyujin <kyujinlee82@gmail.com>
2022-08-11 18:40:22 +0900

    게시물 상세보기 기능 구현(LocalDateTime)

--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -97,7 +97,7 @@ public class App {
 			sql.append("ORDER BY id DESC");
 
 			List<Map<String, Object>> articlesListMap = DBUtil.selectRows(conn, sql);
-			
+
 			for (Map<String, Object> articleMap : articlesListMap) {
 				articles.add(new Article(articleMap));
 			}
@@ -107,7 +107,7 @@ public class App {
 				return 0;
 			}
 			System.out.println("번호  /  제목");
-			
+
 			for (Article article : articles) {
 				System.out.printf("%d  /  %s\n", article.id, article.title);
 			}
@@ -116,13 +116,26 @@ public class App {
 			int id = Integer.parseInt(cmd.split(" ")[2]);
 
 			System.out.printf("== %d번 게시물 수정 ==\n", id);
+
+			SecSql sql = new SecSql();
+
+			sql.append("SELECT COUNT(*)");
+			sql.append("FROM article");
+			sql.append(" WHERE id = ?", id);
+
+			int articlesCount = DBUtil.selectRowIntValue(conn, sql);
+
+			if (articlesCount == 0) {
+				System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+				return 0;
+			}
+
 			System.out.printf("새 제목 : ");
 			String title = sc.nextLine();
 			System.out.printf("새 내용 : ");
 			String body = sc.nextLine();
 
-			SecSql sql = new SecSql();
-
+			sql = new SecSql();
 			sql.append("UPDATE article");
 			sql.append(" SET updateDate = NOW()");
 			sql.append(", title = ?", title);
@@ -139,25 +152,51 @@ public class App {
 			System.out.println("== 게시물 삭제 ==");
 
 			SecSql sql = new SecSql();
-			
+
 			sql.append("SELECT COUNT(*)");
 			sql.append("FROM article");
 			sql.append(" WHERE id = ?", id);
-			
+
 			int articlesCount = DBUtil.selectRowIntValue(conn, sql);
-			
+
 			if (articlesCount == 0) {
 				System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 				return 0;
 			}
-			
+
 			sql = new SecSql();
 			sql.append("DELETE FROM article");
 			sql.append(" WHERE id = ?", id);
-			
+
 			DBUtil.delete(conn, sql);
-			
+
 			System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
+
+		} else if (cmd.startsWith("article detail ")) {
+			int id = Integer.parseInt(cmd.split(" ")[2]);
+
+			SecSql sql = new SecSql();
+
+			sql.append("SELECT *");
+			sql.append("FROM article");
+			sql.append(" WHERE id = ?", id);
+
+			Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+
+			if (articleMap.isEmpty()) {
+				System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+				return 0;
+			}
+			
+			System.out.printf("== %d번 게시물 상세보기 ==\n", id);
+			
+			Article article = new Article(articleMap);
+			
+			System.out.printf("번호 : %d\n", article.id);
+			System.out.printf("작성날짜 : %s\n", article.regDate);
+			System.out.printf("수정날짜 : %s\n", article.updateDate);
+			System.out.printf("제목 : %s\n", article.title);
+			System.out.printf("내용 : %s\n", article.body);
 
 		} else {
 			System.out.println("존재하지 않는 명령어입니다.");
