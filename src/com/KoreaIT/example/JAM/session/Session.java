30b057e708b8f3fd82b17bdc77bb55ebf17c2a27
Leekyujin <kyujinlee82@gmail.com>
2022-08-17 21:07:09 +0900

    member profile 기능 추가, 로그인 시도 횟수 추가

--- src/com/KoreaIT/example/JAM/session/Session.java
+++ src/com/KoreaIT/example/JAM/session/Session.java
@@ -1,5 +1,13 @@
 package com.KoreaIT.example.JAM.session;
 
+import com.KoreaIT.example.JAM.Member;
+
 public class Session {
+	public int loginedMemberId;
+	public Member loginedMember;
+	
+	public Session() {
+		loginedMemberId = -1;
+	}
 
 }
